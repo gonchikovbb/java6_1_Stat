4d242c808c67678e3c2677bb541d48b518c612d0
Bolot Gonchikov <gonchikovbb@gmail.com>
2022-03-16 12:19:49 +0300

    add 6 methods

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -5,10 +5,83 @@ public class StatsService {
     public int calcSum(int[] sales) {
         int sum = 0;
         for (int sale : sales) {
-            sum +=sale;
+            sum += sale;
         }
         return sum;
     }
 
+    public int calcMidSum(int[] sales) {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        sum /= sales.length;
+        return sum;
+    }
 
+    public int maxSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце maxMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale >= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    public int monLessMidSales(long[] sales) {
+        int sum = 0;
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            sum += sale;
+        }
+        sum /= sales.length;
+
+        int minMonth = 0;
+        int count = 0;
+        for (long sale : sales) {
+            if (sale < sum) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int monMoreMidSales(long[] sales) {
+        int sum = 0;
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            sum += sale;
+        }
+        sum /= sales.length;
+
+        int minMonth = 0;
+        int count = 0;
+        for (long sale : sales) {
+            if (sale > sum) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -13,6 +13,58 @@ class StatsServiceTest {
         int expected = 180;
         int actual = service.calcSum(managerSales);
 
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcMidSum() {
+        StatsService service = new StatsService();
+        int[] managerSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+        int actual = service.calcMidSum(managerSales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMaxSales() {
+        StatsService service = new StatsService();
+        long[] managerSales = {8, 15, 13, 0, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+        long actual = service.maxSales(managerSales);
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void shouldMinSales() {
+        StatsService service = new StatsService();
+        long[] managerSales = {8, 15, 13, -15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 4;
+        long actual = service.minSales(managerSales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMonLessMidSales() {
+        StatsService service = new StatsService();
+        long[] managerSales = {8, 15, 13, 0, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 3;
+        long actual = service.monLessMidSales(managerSales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldMonMoreMidSales() {
+        StatsService service = new StatsService();
+        long[] managerSales = {8, 15, 13, 1000, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 1;
+        long actual = service.monMoreMidSales(managerSales);
+
+        assertEquals(expected, actual);
     }
 }
